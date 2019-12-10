f40da6eafea28d9934e37abc67410ba109acb913
jaluik <cqzhengmouren@gmail.com>
2019-12-10 17:31:42 +0800

    求字符串最大子串长度

---
+++ LeetCode/findLongestSubstring.js
@@ -0,0 +1,17 @@
+var lengthOfLongestSubstring = function(s) {
+  let num = 0,
+    res = 0;
+  let m = "";
+  for (n of s) {
+    if (m.indexOf(n) == -1) {
+      m += n;
+      num++;
+      res = res < num ? num : res;
+    } else {
+      m += n;
+      m = m.slice(m.indexOf(n) + 1);
+      num = m.length;
+    }
+  }
+  return res;
+};
